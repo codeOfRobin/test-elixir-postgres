a92213961acd133abfeb589acaed41aa9c937c0a
Robin Malhotra <me@rmalhotra.com>
2020-10-31 01:36:34 +0000

    Update database names
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

--- apps/auction/config/prod.exs
+++ apps/auction/config/prod.exs
@@ -11,7 +11,9 @@ config :auction, Auction.Repo, database: "auction",
   adapter: Ecto.Adapters.Postgres,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true,
-  url: System.get_env("DATABASE_URL")
+  username: System.get_env("DATABASE_USERNAME"),
+  password: System.get_env("DATABASE_PASSWORD"),
+  hostname: System.get_env("DATABASE_URL"),
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 
