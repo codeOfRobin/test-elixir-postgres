9dbe479f88c58c743fde0408337851934a05825a
Robin Malhotra <me@rmalhotra.com>
2020-10-31 01:14:16 +0000

    Add build script
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

---
+++ build.sh
@@ -0,0 +1,15 @@
+
+mix deps.get --only prod
+MIX_ENV=prod mix compile
+
+
+npm install --prefix ./assets
+npm run deploy --prefix ./assets
+mix phx.digest
+
+
+rm -rf "_build"
+MIX_ENV=prod mix release
+
+
+MIX_ENV=prod mix ecto.migrate 
