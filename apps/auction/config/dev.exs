af700435fd7b1f9c76c46a0bf4e9196822a942ab
Robin Malhotra <me@rmalhotra.com>
2020-10-31 01:44:01 +0000

    Update config
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

--- apps/auction/config/dev.exs
+++ apps/auction/config/dev.exs
@@ -1,7 +1,9 @@
 use Mix.Config
 
 config :auction, Auction.Repo, database: "auction",
-  username: "test_user",
-  password: "workbox-rodeo-forecast-rectify-taxiderm",
-  hostname: "localhost",
-  port: "5432"
+  adapter: Ecto.Adapters.Postgres,
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true,
+  username: System.get_env("DATABASE_USERNAME"),
+  password: System.get_env("DATABASE_PASSWORD"),
+  hostname: System.get_env("DATABASE_URL")
