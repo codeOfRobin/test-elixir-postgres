f1e2591cbc38f9a6d4ee6bce80468a52ccee761d
Robin Malhotra <me@rmalhotra.com>
2020-10-31 01:08:50 +0000

    Fix recursive import
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

--- apps/auction/config/prod.exs
+++ apps/auction/config/prod.exs
@@ -14,4 +14,4 @@ config :auction, Auction.Repo, database: "auction",
   url: System.get_env("DATABASE_URL")
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env()}.exs"
+
