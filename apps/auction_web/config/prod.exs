16759b0df1384aed9251a2abeb509d4e68e90981
Robin Malhotra <me@rmalhotra.com>
2020-10-31 01:09:47 +0000

    Fix config
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

--- apps/auction_web/config/prod.exs
+++ apps/auction_web/config/prod.exs
@@ -16,9 +16,6 @@ use Mix.Config
 config :auction_web, AuctionWeb.Endpoint,
   load_from_system_env: true,
   url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME")],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # ## SSL Support
 #
